cf03f4ec6ccdef54e8bce73a59c8f9d4e4b20186
Fs <Fsu0413@vip.qq.com>
2023-01-07 08:26:56 +0800

    Revert "这个配置也编不过了"
    This reverts commit 302da698628010e8b22581630f676d38871257df.

--- Compile/lib/qtCompile/conf.lua
+++ Compile/lib/qtCompile/conf.lua
@@ -3578,7 +3578,8 @@ conf.q6_4wx6v2 = {
 		-DFEATURE_sql_sqlite=ON
 		-DFEATURE_sql_odbc=ON
 		-DFEATURE_system_sqlite=OFF
-		-DBUILD_qtwebengine=OFF
+		-DFEATURE_webengine_proprietary_codecs=ON
+		-DFEATURE_qtpdf_build=ON
 		-DBUILD_qtopcua=OFF
 	]],
 }

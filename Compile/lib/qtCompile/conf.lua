806abc58b2c4f474fbce135365718d6a65bd6ce3
Fs <Fsu0413@vip.qq.com>
2024-02-15 11:29:39 +0800

    Revert "你妈"
    This reverts commit 2a3f35d6a5ad0093f091b12dfd54fa6b050793d8.

--- Compile/lib/qtCompile/conf.lua
+++ Compile/lib/qtCompile/conf.lua
@@ -5618,7 +5618,7 @@ conf.q6_6wx6v2 = {
 		-DMySQL_INCLUDE_DIR="&MYSQLPREFIX&\include\mariadb"
 		-DMySQL_LIBRARY="&MYSQLPREFIX&\lib\mariadb\libmariadb.lib"
 		-DFEATURE_system_sqlite=OFF
-		-DBUILD_qtwebengine=OFF
+		-DFEATURE_webengine_proprietary_codecs=ON
 		-DFEATURE_qtpdf_build=ON
 		-DBUILD_qtgrpc=OFF
 	]],

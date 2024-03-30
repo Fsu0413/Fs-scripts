32a1c7d9946bc4227f8b1fbaa3fdf7fb80023704
Fs <Fsu0413@vip.qq.com>
2024-03-30 08:38:21 +0800

    HostOsVer support Linux and macOS, support running from MSYS2 (will make use of it NOW!)

--- Compile/lib/HostOsVer.lua
+++ Compile/lib/HostOsVer.lua
@@ -1,8 +1,9 @@
 
 local hostOsVer = {}
 
-hostOsVer.Windows = function()
-	local file, err = io.popen("cmd /c ver" , "r")
+local winCommon = function(...)
+	local file, err = ...
+
 	if not file then return fail, err end
 
 	-- the version string is in the second line
@@ -51,6 +52,12 @@ hostOsVer.Windows = function()
 		end
 	end
 
+	if not prefix then
+		return fail, "Windows version not supported " .. major .. "." .. minor .. "." .. patch
+	end
+
+	-- Currently not presented so comment out
+	--[==[
 	-- known Windows pre-7 Service Packs / Windows 10~11 codename
 	local codename = {
 		["6001"]  = "Service Pack 1", -- Windows Vista
@@ -81,15 +88,137 @@ hostOsVer.Windows = function()
 		["25398"] = "23H2", -- What is this... Windows Server 23H2
 	}
 
-	if not prefix then
-		return fail, "Windows version not supported " .. line
-	end
-
 	if codename[patch] then
 		prefix = prefix .. " " .. codename[patch]
 	end
+	]==]
 
 	return prefix .. " " .. major .. "." .. minor .. "." .. patch
 end
+
+hostOsVer.win = function()
+	-- Just "cmd /c ver" won't work under MSYS2 since MSYS2 replaces "/c" to "C:/"
+	-- "ver" is Windows cmd builtin (What the hell!?) and there is no "ver.exe" at all in Windows
+	-- but if "ver" or "ver.exe" is run under cmd it works magically
+	return winCommon(io.popen("cmd /c ver" , "r"))
+end
+
+hostOsVer.winArm = hostOsVer.win
+
+hostOsVer.msys = function()
+	-- Workaround is setting environment variable MSYS2_ARG_CONV_EXCL before cmd is run
+	return winCommon(io.popen("MSYS2_ARG_CONV_EXCL='*' cmd /c ver" , "r"))
+end
+
+hostOsVer.linux = function()
+	-- /etc/os-release is suggested by the systemd guys, and is becoming the de facto standard for checking the Linux distro information.
+	-- Since currently only Rocky Linux is in my build environment and it is already using systemd, /etc/os-release must be provided.
+	-- Other OSes like FreeBSD is also using this file. (but macOS is not using it!)
+
+	-- There is a blog of this file - https://0pointer.de/blog/projects/os-release
+	-- The documentation of this file is - https://www.freedesktop.org/software/systemd/man/latest/os-release.html
+	local file
+	for _, fileName in ipairs({
+		"/etc/os-release",
+		"/usr/lib/os-release"
+	}) do
+		local err
+		file, err = io.open(fileName, "r")
+		if file then break end
+	end
+
+	if not file then return fail, "/etc/os-release series of file are not readable" end
+
+	-- It is mainly for easily parsed by shell scripts, but I'm parsing this file using Lua....
+	-- So, let's use pattern for parsing it
+	local osRelease = {}
+
+	for l in file:lines() do
+		-- Each line have NAME_KEY="Value" format
+		-- The key can contain only capital character and underscore (Pretty Okay!)
+		-- The value may be escaped / quoted in any manner (What the hell?!)
+		-- But since we need only NAME and VERSION_ID field this may not be that complicated
+		local k, v = string.match(l, "^([A-Z_]+)=(.+)$")
+		if k then
+			-- Simply remove quote if needed, since NAME and VERSION_ID shouldn't be too complicated I think...
+			if string.sub(v, 1, 1) == string.sub(v, -1) then
+				if (string.sub(v, 1, 1) == "\"") or (string.sub(v, 1, 1) == "'") then
+					v = string.sub(v, 2, -2)
+				end
+			end
+			osRelease[k] = v
+		end
+	end
+
+	-- Parse complete, and finally we can output result!
+	local ret = osRelease.NAME
+	if osRelease.VERSION_ID then
+		ret = ret .. " " .. osRelease.VERSION_ID
+	end
+
+	return ret
+end
+
+hostOsVer.macOS = function()
+	-- On macOS / OS X / Mac OS X there is a command called "sw_vers"
+	-- It exists so long that It should not be needed to check if it exists, just using it is possible
+
+	local file, err = io.popen("sw_vers -productVersion", "r")
+
+	if not file then return fail, err end
+
+	-- The version string is in the first line
+	local line = file:read()
+	file:close()
+
+	-- macOS system version may not contain the patch digit!
+	local major, minor, patch = string.match(line, "^(%d+)%.(%d+)%.(%d+)$")
+	if not major then major, minor = string.match(line, "^(%d+)%.(%d+)$") end
+	if not major then return fail, "output from sw_vers not reconized: " .. line end
+
+	local prefix
+
+	-- Shall we ever support Mac OS 9 and before....???? This command may not work back in these days
+	if tonumber(major) < 7 then
+		prefix = "System " .. major
+	elseif tonumber(major) == 7 then
+		if tonumber(minor) < 5 then
+			prefix = "System " .. major
+		elseif tonumber(minor) == 5 then
+			if not patch then
+				prefix = "System " .. major
+			else
+				prefix = "Mac OS " .. major
+			end
+		else
+			prefix = "Mac OS " .. major
+		end
+	elseif tonumber(major) < 10 then
+		prefix = "Mac OS " .. major
+	elseif tonumber(major) == 10 then
+		if tonumber(minor) < 8 then
+			-- 10.0 to 10.7 are prefixed "Mac OS X"
+			prefix = "Mac OS X"
+		elseif tonumber(minor) < 12 then
+			-- 10.8 to 10.11 are prefixed "OS X"
+			prefix = "OS X"
+		elseif tonumber(minor) < 17 then
+			-- 10.12 and later are prefixed "macOS"
+			-- There may be macOS 10.16 if our Lua is not rebuilt using macOS 11 or later SDK.
+			prefix = "macOS"
+		end
+	else
+		-- It is not expected to change in near future
+		prefix = "macOS"
+	end
+
+	if not prefix then
+		return fail, "macOS version not supported " .. line
+	end
+
+	return prefix .. " " .. major .. "." .. minor .. (patch and ("." .. patch) or "")
+end
+
+hostOsVer.macM1 = hostOsVer.mac
 
 return hostOsVer
